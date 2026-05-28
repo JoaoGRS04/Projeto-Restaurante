ac10932162f9575cf570add1d568f9bd6dba71ab
JoaoGRS04 <joaoshowgabriel@gmail.com>
2026-05-28 20:53:46 -0300

    fix: fixing bugs

--- main.py
+++ main.py
@@ -41,58 +41,67 @@ def salvar_sobremesas(sobremesas):
     with open(ARQUIVO_SOBREMESAS, "w", encoding="utf-8") as arquivo:
         json.dump(sobremesas, arquivo, indent=4, ensure_ascii=False)
 
-def funcionario():
-   print("1 - adcionar prato: ")
-   print("2 - remover prato: ")
-   print("3 - editar preco: ")
-   escolha = print("o que voce deseja? ")
 
-   match escolha:
-       case "1":
-           addprato()
-       case "2":
-           removeprato()
-       case "3":
-           editpreco()
+entradas = carregar_entradas()
+pratos_principais = carregar_pratos_principais()
+sobremesas = carregar_sobremesas()
+
+
+def funcionario():
+    os.system('cls')
+    print("---  AREA DO FUNCIONÁRIO  ---\n")
+    print("1 - Adcionar prato: ")
+    print("2 - Remover prato: ")
+    print("3 - Editar preco: ")
+    escolha = int(input("\nO que voce deseja? "))
+
+    match escolha:
+        case 1:
+            addprato()
+        case 2:
+            removeprato()
+        case 3:
+            editpreco()
 
 
 def catprato():
-    print("1 -  entrada")
-    print("2 - prato principal")
-    print("3 -  sobremesa")
-    cat = input("Escolha a categoria")
+    print("\n1 - Entrada")
+    print("2 - Prato principal")
+    print("3 - Sobremesa")
+    cat = input("\nEscolha a categoria: ")
     return cat
 
 
 def addprato():
-    match cat:
+    match catprato():
         case "1":
             entrada = {}
             entrada["Nome"] = input("Digite o nome da entrada: ")
-            entrada["Preco"] = input("Digite o preco da entrada: ")
+            entrada["Preco"] = float(input("Digite o preco da entrada: "))
             entrada["Descricao"] = input("Digite a descricao: ")
             entradas.append(entrada)
             salvar_entradas(entradas)
 
         case "2":
             prato_principal = {}
-            prato_principal["Nome"] = input("Digite o nome do prato_principal: ")
-            prato_principal["Preco"] = input("Digite o preco do prato principal: ")
-            prato_principal["Descricao"] = input("Digite a descricao do prato principal: ")
+            prato_principal["Nome"] = input(
+                "Digite o nome do prato principal: ")
+            prato_principal["Preco"] = float(
+                input("Digite o preco do prato principal: "))
+            prato_principal["Descricao"] = input(
+                "Digite a descricao do prato principal: ")
             pratos_principais.append(prato_principal)
             salvar_pratos_principais(pratos_principais)
-
 
         case "3":
             sobremesa = {}
             sobremesa["Nome"] = input("Digite o nome da sobremsa: ")
-            sobremesa["Preco"] = input("Digite o preco da sobremesa: ")
+            sobremesa["Preco"] = float(input("Digite o preco da sobremesa: "))
             sobremesa["Descricao"] = input("Digite a descricao: ")
-            sobremesas.append(entrada)
-            salvar_entradas(sobremesas)
+            sobremesas.append(sobremesa)
+            salvar_sobremesas(sobremesas)
 
-   
-            
+
 while True:
     print("\n ---------------------------")
     print("|        BEM-VINDO AO       |")
@@ -103,16 +112,15 @@ while True:
     print("[2] - Sou funcionário")
     print("[3] - Sair")
 
-    opcao = input("Escolha uma das opções acima: ")
+    opcao = int(input("Escolha uma das opções acima: "))
 
     match opcao:
-        case "1":
-            print("\n-----  MENU  ----")
-            # cliente()
-        case "2":
-            print("\n---  AREA DO FUNCIONÁRIO  ---")
-            # funcionario()
-        case "3":
+        case 1:
+            cliente_cardapio()
+        case 2:
+            funcionario()
+        case 3:
             print("\nObrigado pela visita!")
+            break
         case _:
             print("\nOpção inválida!")
