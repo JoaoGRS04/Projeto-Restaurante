551cea3ceb540da21b8c462f3b6e4ae525992ea0
pedrovmura <pedrovmura@gmail.com>
2026-05-28 18:06:23 -0300

    chore: add first interface

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+cardapio {}
+
+# def cliente 
+
+# def funcionario
+
+while True:
+    print("\n ---------------------------")
+    print("|        BEM-VINDO AO       |")
+    print("|      Bandeijão da PUC     |")
+    print(" ---------------------------\n")
+
+    print("[1] - Sou cliente")
+    print("[2] - Sou funcionário")
+    print("[3] - Sair")
