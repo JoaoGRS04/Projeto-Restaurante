2f85f4bb1791ba5955a4e3a3f908d8d4daac5622
Gabriel almeida <algabriel0609@gmail.com>
2026-05-28 19:48:35 -0300

    feat: addprato function

--- main.py
+++ main.py
@@ -41,7 +41,57 @@ def salvar_sobremesas(sobremesas):
     with open(ARQUIVO_SOBREMESAS, "w", encoding="utf-8") as arquivo:
         json.dump(sobremesas, arquivo, indent=4, ensure_ascii=False)
 
+def funcionario():
+   print("1 - adcionar prato: ")
+   print("2 - remover prato: ")
+   print("3 - editar preco: ")
+   escolha = print("o que voce deseja? ")
 
+   match escolha:
+       case "1":
+           addprato()
+       case "2":
+           removeprato()
+       case "3":
+           editpreco()
+
+
+def catprato():
+    print("1 -  entrada")
+    print("2 - prato principal")
+    print("3 -  sobremesa")
+    cat = input("Escolha a categoria")
+    return cat
+
+def addprato():
+    match cat:
+        case "1":
+            entrada = {}
+            entrada["Nome"] = input("Digite o nome da entrada: ")
+            entrada["Preco"] = input("Digite o preco da entrada: ")
+            entrada["Descricao"] = input("Digite a descricao: ")
+            entradas.append(entrada)
+            salvar_entradas(entradas)
+
+        case "2":
+            prato_principal = {}
+            prato_principal["Nome"] = input("Digite o nome do prato_principal: ")
+            prato_principal["Preco"] = input("Digite o preco do prato principal: ")
+            prato_principal["Descricao"] = input("Digite a descricao do prato principal: ")
+            pratos_principais.append(prato_principal)
+            salvar_pratos_principais(pratos_principais)
+
+
+        case "3":
+            sobremesa = {}
+            sobremesa["Nome"] = input("Digite o nome da sobremsa: ")
+            sobremesa["Preco"] = input("Digite o preco da sobremesa: ")
+            sobremesa["Descricao"] = input("Digite a descricao: ")
+            sobremesas.append(entrada)
+            salvar_entradas(sobremesas)
+
+   
+            
 while True:
     print("\n ---------------------------")
     print("|        BEM-VINDO AO       |")
